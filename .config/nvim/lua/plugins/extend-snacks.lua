959e63e0b8e2cc536c8eb324346dd97f9b56202c
Gerrit Wanderer <gerrit.wanderer@gmail.com>
2025-12-24 21:45:14 +0100

    extend snacks scratchpad with a keybind to show my daily notes from obsidian

--- .config/nvim/lua/plugins/extend-snacks.lua
+++ .config/nvim/lua/plugins/extend-snacks.lua
@@ -2,6 +2,42 @@ return {
 	"folke/snacks.nvim",
 	keys = {
 		{
+			"<leader>dn", -- or whatever key you prefer
+			function()
+				-- Get date components
+				local year = os.date("%Y")
+				local month_num = os.date("%m")
+				local month_name = os.date("%B")
+				local day_name = os.date("%A")
+				local full_date = os.date("%Y-%m-%d")
+
+				-- Build the path matching your structure
+				local notes_dir = vim.fn.expand("~/Workspace/notes")
+				local daily_note = string.format(
+					"%s/00-Daily/%s/%s-%s/%s-%s.md",
+					notes_dir,
+					year,
+					month_num,
+					month_name,
+					full_date,
+					day_name
+				)
+
+				Snacks.scratch({
+					file = daily_note,
+					ft = "markdown",
+					name = "Daily Note",
+					autowrite = true,
+					filekey = {
+						cwd = false,
+						branch = false,
+						count = false,
+					},
+				})
+			end,
+			desc = "Open Daily Note",
+		},
+		{
 			"<leader>sr",
 			function()
 				Snacks.picker.resume()
