15ac736eff22bd93742a7c4400db1e99f9c123e5
Gerrit Wanderer <gerrit.wanderer@on-running.com>
2026-02-25 16:41:10 +0100

    disable animations for intendscope

--- .config/nvim-minimax/plugin/30_mini.lua
+++ .config/nvim-minimax/plugin/30_mini.lua
@@ -504,7 +504,11 @@ end)
 --
 -- See also:
 -- - `:h MiniIndentscope.gen_animation` - available animation rules
-later(function() require('mini.indentscope').setup() end)
+later(function()
+  require('mini.indentscope').setup({
+    draw = { animation = require('mini.indentscope').gen_animation.none() },
+  })
+end)
 
 -- Jump to next/previous single character. It implements "smarter `fFtT` keys"
 -- (see `:h f`) that work across multiple lines, start "jumping mode", and
